4b41232a0d248143d25bf37205ae031b8d86085e
Matthew Burns <msb@ameritech.net>
2023-02-06 16:20:28 -0500

    Add friendship model

---
+++ db/migrate/20230206200911_create_friendships.rb
@@ -0,0 +1,14 @@
+class CreateFriendships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false
+      t.boolean :friendship_status
+
+
+      t.timestamps
+    end
+
+    add_foreign_key :friendships, :users, column: :friend_id
+  end
+end
