b9d93d6936fb8904ecc8e485274feba81e1a65bb
Matthew Burns <msb@ameritech.net>
2023-02-09 08:29:25 -0500

    Update user update action & user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :about]
+  before_action :set_user, only: [:show, :edit, :about, :update, :destroy]
 
   def index
     @users = User.all.order(id: :asc)
@@ -14,7 +14,24 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password, :first_name, :last_name)
+    end
 
     def set_user
       @user = User.find_by(username: params[:username])
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get ":username", to: "users#show", as: "user"
   get ":username/edit", to: "users#edit", as: "user_edit"
   get ":username/about", to: "users#about", as: "user_about"
+  patch ":username", to: "users#update", as: "user_update"
+  delete ":username", to: "users#destroy", as: "user_destroy"
 
   resources :posts do
     resources :comments, only: [:create, :update, :delete]
